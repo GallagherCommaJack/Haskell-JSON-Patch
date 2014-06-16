e06fdbd618d8625f4f34081f62cf995bc9616039
Jack Gallagher <jack@gallabytes.com>
2014-06-16 15:11:23 -0700

    Added full patching to TreePatch!
    Now it can do *all* the patches.  I’ll make it conform to the spec
    later, for now I just wanted a good approximation.

--- TreePatch.hs
+++ TreePatch.hs
@@ -3,6 +3,7 @@ module TreePatch where
 import Data.Tree
 import Control.Arrow (first, second)
 import Control.Applicative ((<$>), (<*>))
+import Data.Monoid
 import Data.Text.Lazy (Text(..), unpack)
 import qualified Data.Text.Lazy as T hiding (copy, replace)
 
@@ -26,19 +27,44 @@ findAndDelete _ [] = (Nothing,[])
 
 findInForest x = findAndDelete ((==x) . rootLabel)
 
-patch :: Operation -> Tree T.Text -> Tree T.Text
+toPath :: Text -> Path
+toPath = T.split (=='/')
+
+fromPath :: Path -> Text
+fromPath = T.concat . fmap (flip T.snoc '/')
+
+findAndDeleteAtPath :: Path -> Tree Text -> (Maybe (Tree Text),Tree Text)
+findAndDeleteAtPath (x:xs) tr@(Node i ts) = case (xs,findInForest x ts) of
+  ([],(Just n, tts)) -> (Just n, Node i tts)
+  ([],(Nothing, _))  -> (Nothing, tr)
+  (_, (Just n, tts)) -> second (Node i . (:tts)) $ findAndDeleteAtPath xs n
+  (_, (Nothing, _))  -> (Nothing, tr)
+findAndDeleteAtPath [] tr@(Node i ts) = (Just tr, leaf i)
+
+patch :: Operation -> Tree Text -> Either Text (Tree Text)
 patch (Add (x:xs) val) tr@(Node i ts) = case (xs,findInForest x ts) of
-  ([],(Just k, tts)) -> Node x $ leaf val : tts
-  ([],(Nothing, _))  -> Node x $ leaf val : ts
-  (_, (Just n, tts)) -> Node i $ patch (Add xs val) n : tts
-  (_, (Nothing, _))  -> tr
-patch (Add [] val) (Node i ts) = Node i [leaf val]
+  ([],(Just k, tts)) -> Right $ Node x $ leaf val : tts
+  ([],(Nothing, _))  -> Right $ Node x $ leaf val : ts
+  (_, (Just n, tts)) -> Node i <$> (:tts) <$> patch (Add xs val) n
+  (_, (Nothing, _))  -> Right tr
+patch (Add [] val) (Node i ts) = Right $ Node i [leaf val]
 
-patch (Rem (x:xs)) tr@(Node i ts) = case (xs,findInForest x ts) of
-  ([],(Just n, tts)) -> Node i tts
-  ([],(Nothing, _))  -> tr
-  (_, (Just n, tts)) -> Node i $ patch (Rem xs) n : tts
-  (_, (Nothing, _))  -> tr
-patch (Rem []) (Node i ts) = leaf i
+patch (Rem []) (Node i ts) = Right $ leaf i
+patch (Rem xs) tr = Right $ snd $ findAndDeleteAtPath xs tr
 
-patch _ _ = undefined
+patch (Cop p1 p2) tr = case fst $ findAndDeleteAtPath p1 tr of
+  (Just v) -> patch (Add p2 $ rootLabel v) tr
+  Nothing  -> Left $ "Could not find value at path " <> fromPath p1
+
+patch (Mov p1 p2) tr = case findAndDeleteAtPath p1 tr of
+  (Just v, trr) -> patch (Add p2 $ rootLabel v) trr
+  (Nothing, _)  -> Left $ "Could not find value at path " <> fromPath p1
+
+patch (Rep p v) tr = case findAndDeleteAtPath p tr of
+  (Just v, trr) -> patch (Add p $ rootLabel v) trr
+  (Nothing, _)  -> Left $ "Could not find value at path " <> fromPath p
+
+patch (Tes p t) tr = case fst $ findAndDeleteAtPath p tr of
+  (Just (Node i _)) -> if i == t then Right tr else Left $ "Test failed, \n" <>
+                                                    i <> " \\= " <> t
+  Nothing -> Left $ "Test failed, couldn't find value at " <> fromPath p
