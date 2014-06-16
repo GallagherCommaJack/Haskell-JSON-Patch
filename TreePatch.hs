0cea5b4afaa1e8671a462ef2510b5590000a84ef
Jack Gallagher <jack@gallabytes.com>
2014-06-16 00:07:13 -0700

    Added Remove and Add to TreePatch
    First two Patch operations are ready!

--- TreePatch.hs
+++ TreePatch.hs
@@ -1,6 +1,7 @@
 module TreePatch where
 
 import Data.Tree
+import Control.Arrow (first, second)
 import Control.Applicative ((<$>), (<*>))
 import Data.Text.Lazy (Text(..), unpack)
 import qualified Data.Text.Lazy as T hiding (copy, replace)
@@ -16,3 +17,28 @@ data Operation = Add Path Text
                | Rep Path Text
                | Tes Path Text
                deriving (Show, Read, Eq)
+
+findAndDelete :: (a -> Bool) -> [a] -> (Maybe a,[a])
+findAndDelete f (x:xs) = if f x
+                         then (Just x,xs)
+                         else second (x:) $ findAndDelete f xs
+findAndDelete _ [] = (Nothing,[])
+
+findInForest x = findAndDelete ((==x) . rootLabel)
+
+patch :: Operation -> Tree T.Text -> Tree T.Text
+patch (Add (x:xs) val) tr@(Node i ts) = case (xs,findInForest x ts) of
+  ([],(Just k, tts)) -> Node x $ leaf val : tts
+  ([],(Nothing, _))  -> Node x $ leaf val : ts
+  (_, (Just n, tts)) -> Node i $ patch (Add xs val) n : tts
+  (_, (Nothing, _))  -> tr
+patch (Add [] val) (Node i ts) = Node i [leaf val]
+
+patch (Rem (x:xs)) tr@(Node i ts) = case (xs,findInForest x ts) of
+  ([],(Just n, tts)) -> Node i tts
+  ([],(Nothing, _))  -> tr
+  (_, (Just n, tts)) -> Node i $ patch (Rem xs) n : tts
+  (_, (Nothing, _))  -> tr
+patch (Rem []) (Node i ts) = leaf i
+
+patch _ _ = undefined
