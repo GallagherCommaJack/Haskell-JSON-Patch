5016135523e17aedae11a4153edce49d76b0ae29
Jack Gallagher <jack@gallabytes.com>
2014-06-16 23:01:57 -0700

    Removed unnecessary operations from parsing Parsing AST now goes directly to Value instead of waiting for another conversion

--- ParseTree.hs
+++ ParseTree.hs
@@ -2,17 +2,20 @@
 
 module ParseTree where
 
-import Data.Aeson (Value(..), decode)
+import Data.Attoparsec.ByteString.Lazy (parse, maybeResult)
+import Data.Aeson (Value(..), json)
 
-import Data.Tree
+import Data.Tree (Tree(..))
 
 import Data.Text.Lazy (Text(..))
 import Data.Text.Lazy.Encoding (encodeUtf8)
 
-import qualified Data.ByteString.Lazy.Char8 as BS
-import qualified Data.Text.Lazy             as T
+import Data.Maybe (fromJust)
+
+import qualified Data.ByteString.Lazy.Char8 as BS (ByteString(..), pack, readFile)
+import qualified Data.Text.Lazy             as T (fromStrict, pack)
 import qualified Data.Text.Internal         as Strict (Text(..))
-import qualified Data.HashMap.Strict        as Strict
+import qualified Data.HashMap.Strict        as Strict (toList)
 
 leaf :: a -> Tree a
 leaf = flip Node []
@@ -36,13 +39,16 @@ objToText Null = ""
 objToText _ = error "Invalid obj"
 
 bsToTree :: BS.ByteString -> Maybe (Tree Text)
-bsToTree = fmap objToTree . decode
+bsToTree = fmap objToTree . maybeResult . parse json
 
 tToTree :: Text -> Maybe (Tree Text)
 tToTree = bsToTree . encodeUtf8
 
 strToTree :: String -> Maybe (Tree Text)
 strToTree = bsToTree . BS.pack
+
+parseFile :: String -> IO (Tree Text)
+parseFile = fmap fromJust . fmap bsToTree . BS.readFile
 
 jsonTest :: BS.ByteString
 jsonTest = BS.pack "{ \"a\" : \"abc\", \"foo\" : { \"bar\" : \"baz\", \"qux\" : \"quux\"}}"
