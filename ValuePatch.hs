96a2fb49ee22edf48cd0106ca2937665788a6865
Jack Gallagher <jack@gallabytes.com>
2014-06-22 14:14:43 -0700

    Added ValuePatch
    So, the other representations turned out to be not that helpful (lots
    of O(n) operations, ugliness converting to and from, yuck), so I wrote
    the patches in terms of the internal JSON type

---
+++ ValuePatch.hs
@@ -0,0 +1,92 @@
+module ValuePatch where
+
+import Data.Aeson (Value(..))
+
+import Control.Applicative ((<$>))
+import Data.Monoid ((<>), mconcat)
+
+import Data.Text (Text(..))
+
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as Lazy
+
+import Data.HashMap.Strict (HashMap(..))
+import qualified Data.HashMap.Strict as HM
+
+type Path = [Text]
+
+data Operation = Add Path Value
+               | Rem Path
+               | Cop Path Path
+               | Mov Path Path
+               | Rep Path Value
+               | Tes Path Value
+               deriving (Show, Eq)
+
+objToText :: Value -> Text
+objToText (Object o) = T.pack $ show o
+objToText (Array v) = T.pack $ show v
+objToText (Number n) = T.pack $ show n
+objToText (Bool b) = T.pack $ show b
+objToText (String t) = t
+objToText Null = ""
+
+findAndDelete :: Path -> Value -> Maybe (Value,Value)
+findAndDelete [k] (Object h) =
+  HM.lookup k h >>= \v -> Just (v,Object $ HM.delete k h)
+findAndDelete (x:xs) (Object h) = do
+  sub <- HM.lookup x h
+  (v,new) <- findAndDelete xs sub
+  let deleted = HM.delete x h
+  return (v, Object $ HM.insert x new deleted)
+findAndDelete _ _ = Nothing
+
+--Wouldn't be necessary but Aeson uses strict hashmaps...
+findAtPath :: Path -> Value -> Maybe Value
+findAtPath (k:ks) (Object h) = HM.lookup k h >>= findAtPath ks
+findAtPath [] a = Just a
+
+addAtPath :: Path -> Value -> Value -> Maybe Value
+addAtPath (p:ps) v obj = do
+  (sub, (Object rest)) <- findAndDelete [p] obj
+  patched <- addAtPath ps v sub
+  return $ Object $ HM.insert p patched rest
+addAtPath [] v _ = Just v
+
+toPath :: Text -> Path
+toPath = T.split (=='/')
+
+fromPath :: Path -> Text
+fromPath = mconcat . fmap (flip T.snoc '/')
+
+maybeToEither :: Maybe a -> Either Text a
+maybeToEither (Just a) = Right a
+maybeToEither Nothing = Left "Nothing"
+
+patch :: Operation -> Value -> Either Text Value
+patch (Add p v) obj = case addAtPath p v obj of
+  Just a -> Right a
+  Nothing -> Left $ "Couldn't traverse path " <> fromPath p
+
+patch (Rem p) obj = case snd <$> findAndDelete p obj of
+  (Just a) -> Right a
+  Nothing -> Left $ "Nothing to remove at " <> fromPath p
+
+patch (Cop p1 p2) obj = case findAtPath p1 obj of
+  (Just old) -> patch (Add p2 old) obj
+  Nothing   -> Left $ "Couldn't find value at " <> fromPath p1
+
+patch (Mov p1 p2) obj = do
+  (old,new) <- maybeToEither $ findAndDelete p1 obj
+  patch (Add p2 old) new
+
+patch (Rep p v) obj = case findAndDelete p obj of
+  Nothing -> Left $ "Couldn't find value at path " <> fromPath p
+  _ -> patch (Add p v) obj
+
+patch (Tes p t) obj = case findAtPath p obj of
+  (Just v) ->  if t == v || objToText t == objToText v
+               then return obj
+               else Left $ "Value at path " <> fromPath p <> " is "
+                    <> objToText v <> " not " <> objToText t
+  Nothing -> Left $ "Couldn't find value at path " <> fromPath p
