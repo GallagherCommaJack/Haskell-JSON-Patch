d9b6ace79defc5ceb08a3a45c9eebcca5894fda8
Jack Gallagher <jack@gallabytes.com>
2014-06-22 17:27:01 -0700

    Cleaned up cabal file, added Main
    Tada!  Now generates an executable called patch, first argument is the
    patches, second argument is the file to be patched

---
+++ Main.hs
@@ -0,0 +1,25 @@
+import Data.Aeson (eitherDecode, encode)
+import Control.Applicative ((<$>))
+import Data.Monoid ((<>))
+import Data.Text (unpack)
+
+import System.Environment (getArgs)
+
+import Data.ByteString.Lazy.Char8 as BS (readFile, writeFile)
+
+import ValuePatch (applyPatches)
+import ParsePatch (parsePatchFile)
+
+main = do
+  args <- getArgs
+  case args of
+    (p:fs:_) -> do
+      ops <- parsePatchFile p
+      obj <- eitherDecode <$> BS.readFile fs
+      case obj of (Right o) ->
+                    case applyPatches ops <$> o of
+                      (Right n) -> BS.writeFile fs $ encode n
+                      (Left err) -> error $ unpack err
+                  (Left err) -> error $ "Couldn't parse file " <> fs
+                                <> ", Aeson threw error:\n" <> err
+    _ -> error "Wrong number of arguments"
